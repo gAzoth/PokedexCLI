6bfa1ba648ec867dbbbf6fc91e55dc2f83da41bc
johngaffney66 <johngaffney66@gmail.com>
2025-03-01 10:02:52 -0500

    added cli commands

--- repl.go
+++ repl.go
@@ -1,8 +1,57 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRep1() {
+
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		userInput := words[0]
+
+		command, exists := getCommands()[userInput]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
